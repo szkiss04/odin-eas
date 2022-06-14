57c8b35e0c322274639d37674c197c95101f76d8
szkiss04 <zsoltszilardkiss04@gmail.com>
2022-06-14 14:10:34 +0200

    Final funcionality added, "rainbow" feature added.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# odin-eas
+# The Odin Project: Etch-a-Sketch
 This project is part of 'The Odin Project' curriculum.
+
+This is the final version, with "rainbow" mode. :-)
--- script.js
+++ script.js
@@ -1,20 +1,48 @@
+const pixelContainer = document.getElementById( "pixel-container" );
+const newSketchButton = document.getElementById( "new-sketch" );
+const rainbowModeCheckbox = document.getElementById( "rainbow-mode" );
+
+newSketchButton.addEventListener( "click", newGrid );
+
 function randomColor( ) {
     return `${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}`;
 }
 
+function newGrid( ) {
+    const dimension = prompt("How big sketchpad do you want?");
+    if( dimension < 1 || dimension > 100 ) {
+        alert( "Invalid size! Type a number between 1-100!" );
+        return;
+    }
+    removeGrid( );
+    setupGrid( dimension, pixelContainer );
+}
+
+function removeGrid() {
+    const oldGrid = document.querySelectorAll(".grid-element");
+    oldGrid.forEach( ( element ) => {
+        element.remove();
+    });
+}
+
 function setupGrid( dimension, container ) {
-    container.style.width = `${16 * dimension}px`
     let gridElement;
     for( let i = 0; i < dimension**2; i++ ) {
         gridElement = document.createElement("div");
         gridElement.classList.add( "grid-element" );
+        gridElement.style.width = `${100/dimension}%`;
+        gridElement.style.height = `${100/dimension}%`;
         // gridElement.style.backgroundColor = `rgb(${randomColor()})`;
         container.appendChild( gridElement );
         gridElement.addEventListener("mouseover", function () {
-            this.classList.add( "filled" );
+            if( rainbowModeCheckbox.checked ) {
+                this.style.backgroundColor = `rgb(${randomColor()})`;
+            } else {
+                this.style.removeProperty( "background-color" );
+                this.classList.add( "filled" ); 
+            }
         });   
     }
 }
 
-setupGrid( 16, document.getElementById("pixel-container") );
-
+setupGrid( 16, pixelContainer );
